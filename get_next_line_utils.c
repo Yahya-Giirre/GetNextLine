7bf224d40da03a8d8f776627040e90fd2c3fa48a
Yahya-Giirre <90740531+Yahya-Giirre@users.noreply.github.com>
2022-01-05 12:10:16 +1030

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,6 +1,6 @@
 #include "get_next_line.h"
 
-int  strlen(char *str)
+int  ft_strlen(char *str)
 {
   int i;
   
@@ -8,4 +8,39 @@ int  strlen(char *str)
   while (str[i])
     i++;
     return (i);
+}
+
+char  ft_strchr(const char *str, int c)
+{
+  char  *s;
+  
+  s = (char *)str;
+  if (!str)
+    return (NULL);
+  while (*s && *s != (unsigned char)c)
+    s++;
+  if(*s == (unsigned char)c)
+    return (s);
+  return (NULL);
+}
+
+char  ft_substr(char *s, int start, int len)
+{
+  int i;
+  int j;
+  char  *dest;
+  
+  i = 0;
+  j = 0;
+  dest = (char *)malloc(sizeof(char) * (len + 1));
+  if (!dest)
+    return (NULL);
+  while (s[i])
+  {
+     if (i >= start && j < len)
+       dest[j++] = s[i];
+    i++;
+  }
+  dest[j] = '\0';
+  return (dest);
 }
